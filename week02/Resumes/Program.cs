2cb7d60b5075047953bd857380087974859c4956
MichealOnyemelonu <monyemelonu@gmail.com>
2026-03-10 20:31:58 +0100

    created Job.cs file, wrote the program for it to be public in order for Program.cs to call/communicate with it successfully

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Software Intern";
+        job2._companyName = "Monyetech limited";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        job1.Display();
+        job2.Display();
     }
 }
